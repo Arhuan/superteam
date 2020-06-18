23d1ecd2f81360e5eea6e585ac614553161d098b
Arhuan <aaronhuangwork@gmail.com>
2020-06-17 23:23:50 -0700

    add bias variable

--- models/lin_reg.py
+++ models/lin_reg.py
@@ -3,7 +3,9 @@ from numpy.linalg import solve
 
 class LinearRegression:
     def fit(self, X, y):
+        X = np.c_[X, np.ones(X.shape[0])]
         self.w = solve(X.T@X, X.T@y)
     
     def predict(self, X):
+        X = np.c_[X, np.ones(X.shape[0])]
         return X@self.w 
