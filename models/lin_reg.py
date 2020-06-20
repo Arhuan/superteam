81ee469146d2a7a005a2a0bf04cca67efe8500a1
Arhuan <aaronhuangwork@gmail.com>
2020-06-20 00:00:04 -0700

    trying out rbf kernel

--- models/lin_reg.py
+++ models/lin_reg.py
@@ -1,11 +1,22 @@
 import numpy as np
-from numpy.linalg import solve
 
 class LinearRegression:
+    def gaussian_rbf_kernel(self, X1, X2, sigma=0.5):
+        n, d = np.shape(X1)
+        t, d = np.shape(X2)
+        K = np.zeros((n, t))
+        for i in range(n):
+            for j in range(t):
+                K[i, j] = np.exp(-1 * np.linalg.norm(X1[i] - X2[j])**2 / (2 * sigma**2))
+        return K
+
     def fit(self, X, y):
         X = np.c_[X, np.ones(X.shape[0])]
-        self.w = solve(X.T@X, X.T@y)
+        self.X = X
+        K = self.gaussian_rbf_kernel(X, X)
+        self.w = np.linalg.solve(K.T@K, K.T@y)
     
     def predict(self, X):
         X = np.c_[X, np.ones(X.shape[0])]
-        return X@self.w 
+        K_test = self.gaussian_rbf_kernel(X, self.X)
+        return K_test@self.w 
